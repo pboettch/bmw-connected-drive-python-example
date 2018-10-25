70c5adfd6657efc44e9c90abc90ed29953553842
Patrick Boettcher <patrick.boettcher@posteo.de>
2018-10-25 10:42:47 +0200

    add timeout of 30s to not block infinitely in case of problems

--- bmwcd.py
+++ bmwcd.py
@@ -32,7 +32,7 @@ def main():
 
     if cache is None or cache['expiration'] < time.time():
         # Get access token from authentication server
-        conn = http.client.HTTPSConnection("customer.bmwgroup.com")
+        conn = http.client.HTTPSConnection("customer.bmwgroup.com", timeout=30)
 
         payload = urllib.parse.urlencode( {
             'client_id': 'dbf0a542-ebd1-4ff0-a9a7-55172fbfce35',
@@ -84,7 +84,7 @@ def main():
         access_token = cache['token']
 
     # Get JSON object with vehicle data from backend server
-    conn = http.client.HTTPSConnection("www.bmw-connecteddrive.co.uk")
+    conn = http.client.HTTPSConnection("www.bmw-connecteddrive.co.uk", timeout=30)
 
     headers = {
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
